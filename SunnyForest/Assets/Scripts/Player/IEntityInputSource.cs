5d6476383d4688cbd5a8cf700ae1a92f8ee2575a
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-03-19 23:48:18 +0100

    change input reader as input source

---
+++ SunnyForest/Assets/Scripts/Player/IEntityInputSource.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public interface IEntityInputSource
+    {
+        Vector2 Direction { get; }
+        bool Attack { get; }
+
+        void ResetOneTimeActions();
+    }
+}
