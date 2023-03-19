90514e1b2db0a6d28cf6d7db8870456a13f3d88a
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-03-19 23:49:12 +0100

    add UI source

---
+++ SunnyForest/Assets/Scripts/Player/GameUIInputView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Player
+{
+    public class GameUIInputView : MonoBehaviour, IEntityInputSource
+    {
+        [SerializeField] private Joystick _joystick;
+        [SerializeField] private Button _attackButton;
+        
+        public Vector2 Direction => new Vector2(_joystick.Horizontal, _joystick.Vertical);
+        public bool Attack { get; private set; }
+
+        private void Awake()
+        {
+            _attackButton.onClick.AddListener(() => Attack = true);
+        }
+
+        private void OnDestroy()
+        {
+            _attackButton.onClick.RemoveAllListeners();
+        }
+        
+        public void ResetOneTimeActions()
+        {
+            Attack = false;
+        }
+    }
+}
