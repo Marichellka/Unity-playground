4a63a1279567959b3a1bc342b03bb6635b6909be
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-03-19 23:50:14 +0100

    add player controller for sources

---
+++ SunnyForest/Assets/Scripts/Player/PlayerBrain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerBrain
+    {
+        private readonly PlayerEntity _playerEntity;
+        private readonly List<IEntityInputSource> _inputSources;
+
+        public PlayerBrain(PlayerEntity playerEntity, List<IEntityInputSource> inputSources)
+        {
+            _playerEntity = playerEntity;
+            _inputSources = inputSources;
+        }
+
+        public void OnFixedUpdate()
+        {
+            _playerEntity.Move(GetDirection());
+            if (IsAttack)
+                _playerEntity.StartAttack();
+            
+            foreach (var source in _inputSources)
+                source.ResetOneTimeActions();
+        }
+
+        private Vector2 GetDirection()
+        {
+            foreach (var inputSource in _inputSources)
+            {
+                if (inputSource.Direction.magnitude != 0)
+                    return inputSource.Direction;
+            }
+
+            return new Vector2();
+        }
+
+        private bool IsAttack => _inputSources.Any(source => source.Attack);
+    }
+}
