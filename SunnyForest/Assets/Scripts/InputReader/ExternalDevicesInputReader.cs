0c73aa29e7548c7db041b0af4f3a0a909b36b02f
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-03-22 22:57:30 +0100

    move input readers

---
+++ SunnyForest/Assets/Scripts/InputReader/ExternalDevicesInputReader.cs
@@ -0,0 +1,25 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core.InputReader
+{
+    public class ExternalDevicesInputReader : IEntityInputSource
+    {
+        public Vector2 Direction => new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        public bool Attack { get; private set; }
+
+        public void OnUpdate()
+        {
+            if (!IsPointerOverUI() && Input.GetButtonDown("Fire1"))
+                Attack = true;
+        }
+
+        private bool IsPointerOverUI() => EventSystem.current.IsPointerOverGameObject();
+
+        public void ResetOneTimeActions()
+        {
+            Attack = false;
+        }
+    }
+}
