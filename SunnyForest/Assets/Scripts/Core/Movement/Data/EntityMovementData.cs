629fc716312399e235dd4c4159d4c873cd9aa227
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-03-25 17:36:50 +0100

    add entity movement data

---
+++ SunnyForest/Assets/Scripts/Core/Movement/Data/EntityMovementData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Core.Enums;
+using UnityEngine;
+
+namespace Core.Movement.Data
+{
+    [Serializable]
+    public class EntityMovementData
+    {
+        [field: SerializeField] public float MoveSpeed { get; private set; }
+    }
+}
